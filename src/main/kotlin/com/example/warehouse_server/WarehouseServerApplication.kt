b433fd8433902fe39272fbf7c041a2578484df1d
Proxypepe <sasha.golovin.51@bk.ru>
2021-12-13 22:55:00 +0300

    added procfile

--- src/main/kotlin/com/example/warehouse_server/WarehouseServerApplication.kt
+++ src/main/kotlin/com/example/warehouse_server/WarehouseServerApplication.kt
@@ -1,6 +1,7 @@
 package com.example.warehouse_server
 
 import com.example.warehouse_server.config.SwaggerConfiguration
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Import
@@ -9,8 +10,9 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc
 @SpringBootApplication
 @EnableWebMvc
 @Import(SwaggerConfiguration::class)
-class WarehouseServerApplication
+open class WarehouseServerApplication
 
 fun main(args: Array<String>) {
-    runApplication<WarehouseServerApplication>(*args)
+//    runApplication<WarehouseServerApplication>(*args)
+    SpringApplication.run(WarehouseServerApplication::class.java, *args)
 }
